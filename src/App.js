5d2624b4b194dae90f8eb5fd5addc6b00d6f055f
Seba <sebadalessandro@hotmail.com>
2022-11-25 10:23:01 -0300

    new: styles components and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+//components
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Lista from './components/Lista'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-
+//styles
+import './styles/app.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
