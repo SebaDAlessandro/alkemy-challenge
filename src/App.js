43224d791fb7e299f1813572c492daea6008c628
Seba <sebadalessandro@hotmail.com>
2022-12-06 10:19:21 -0300

    new: button favorite

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Detalle from "./components/detalle/Detalle";
 import Resultados from "./components/resultados/Resultados";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 //styles
 import './styles/app.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+const MySwal = withReactContent(Swal)
+
+
 function App() {
-
-  const favMovies = localStorage.getItem('favs')
-
-  let tempMovieInFavs;
-
-  if(favMovies === null){
-    tempMovieInFavs = []
-  }else{
-    tempMovieInFavs = JSON.parse(favMovies)
-  }
-
+  
   const addOrRemoveFrontFavs = (e)=>{
+    const favMovies = localStorage.getItem('favs')
+  
+    let tempMovieInFavs;
+  
+    if(favMovies === null){
+      tempMovieInFavs = []
+    }else{
+      tempMovieInFavs = JSON.parse(favMovies)
+    }
     const btn = e.currentTarget
     const parent = btn.parentElement
     const imgURL = parent.querySelector('img').getAttribute('src')
@@ -33,8 +38,41 @@ function App() {
     const movieData = {
       imgURL, title, overview, id
     }
-    tempMovieInFavs.push(movieData)
-    localStorage.setItem('favs', JSON.stringify(tempMovieInFavs))
+    let movieIsInArray = tempMovieInFavs.find(oneMovie =>{
+      return oneMovie.id === movieData.id
+    })
+    if(!movieIsInArray){
+      tempMovieInFavs.push(movieData)
+      localStorage.setItem('favs', JSON.stringify(tempMovieInFavs))
+      MySwal.fire({
+        title: 'Success',
+        icon: 'success',
+        text: 'Se ha agregado a favoritos!',
+        confirmButtonText: "Ok",
+    })
+    }else{
+      MySwal.fire({
+        title: 'Are you sure?',
+        text: "Desea quitar esta película de favoritos?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Remove!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          MySwal.fire(
+            'Removed!',
+            'Se ha quitado la película.',
+            'success'
+            )
+            let movieLeft = tempMovieInFavs.filter(oneMovie =>{
+              return oneMovie.id !== movieData.id
+            })
+            localStorage.setItem('favs', JSON.stringify(movieLeft))
+          }
+      })
+    }
   }
 
   return (
